2e48bdcc025976ee9799f063d26d82b9e22181ad
kevin schuchard <schuchard.kevin@gmail.com>
2019-04-24 07:20:45 -0600

    add loggin interceptor

--- src/app/interceptors/index.ts
+++ src/app/interceptors/index.ts
@@ -1,8 +1,16 @@
 /* "Barrel" of Http Interceptors */
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CachingInterceptor } from './cache.interceptor';
+import { LoggingInterceptor } from './logging.interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
+  /**
+   * Order matters: Since interceptors pass request through in the order they're provided,
+   * (requests will flow in A->B->C and responses will flow out C->B->A)
+   * The LoggingInterceptor must be first so that it responds to both network and cache responses.
+   */
+
+  { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true },
 ];
