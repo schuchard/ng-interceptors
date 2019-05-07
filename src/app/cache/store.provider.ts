3c74d9cd5b51b083b717c2a81d6b8119857e652b
kevin schuchard <schuchard.kevin@gmail.com>
2019-05-07 14:25:59 -0600

    store factory

--- src/app/cache/store.provider.ts
+++ src/app/cache/store.provider.ts
@@ -3,5 +3,7 @@ import { InjectionToken } from '@angular/core';
 
 export const CACHE_STORAGE = new InjectionToken<StoreJsAPI>('Cache Storage', {
   providedIn: 'root',
-  factory: () => storage,
+  factory: function store() {
+    return storage;
+  },
 });
